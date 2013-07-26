81e7e9910730c2cf789673c555c026d058a7457d
ptsurko <panek@tut.by>
2013-07-26 16:44:48 +0300

    Added directive for modal dialog

---
+++ App/Controllers/TestController.js
@@ -0,0 +1,18 @@
+﻿angular.module('TestApp')
+    .controller('TestController', function ($scope) {
+        $scope.popup = false;
+        $scope.popup2 = false;
+
+        $scope.confirmOptions = {
+            header: "Modal Dialog",
+            content: "This is a modal window."
+        };
+
+        $scope.togglePopup = function () {
+            $scope.popup = !$scope.popup;
+        };
+        
+        $scope.togglePopup2 = function () {
+            $scope.popup2 = !$scope.popup2;
+        };
+    });
---
+++ App/Modules/Dialog/DialogProvider.js
@@ -0,0 +1,63 @@
+﻿angular.module('ui.directives.dialog', [])
+    .provider('dialog', function () {
+        this.$get = ['$document', function ($document) {
+            var overlayDiv, wrapperDiv, body;
+
+            function dialog() {
+                body = $document.find('body');
+            }
+
+            dialog.prototype.show = function () {
+                wrapperDiv = ensureWrapperExists();
+                wrapperDiv.removeClass('hide');
+            };
+
+            dialog.prototype.modal = function (modalPopup) {
+                showOverlay();
+
+                wrapperDiv = ensureWrapperExists();
+                wrapperDiv.removeClass('hide');
+
+                wrapperDiv.append(modalPopup);
+            };
+
+            dialog.prototype.hide = function () {
+                hideOverlay();
+
+                if (wrapperDiv) {
+                    wrapperDiv.addClass('hide').children().remove();
+                }
+            };
+
+            function ensureWrapperExists() {
+                var wrapDiv = _(body.find('div')).find(function (el) { return angular.element(el).hasClass('modal-wrap'); });
+                if (!wrapDiv) {
+                    body.append((wrapDiv = angular.element('<div class=\'modal-wrap\'></div>')));
+                } else {
+                    wrapDiv = angular.element(wrapDiv);
+                }
+
+                wrapDiv.children().remove();
+                return wrapDiv;
+            }
+
+            function showOverlay() {
+                overlayDiv = _(body.find('div')).find(function (el) { return angular.element(el).hasClass('overlay'); });
+                if (!overlayDiv) {
+                    body.append((overlayDiv = angular.element('<div class=\'overlay hide\'></div>')));
+                } else {
+                    overlayDiv = angular.element(overlayDiv);
+                }
+                
+                overlayDiv.removeClass('hide');
+            }
+
+            function hideOverlay() {
+                if (overlayDiv) {
+                    overlayDiv.addClass('hide');
+                }
+            }
+
+            return new dialog();
+        }];
+    });
